a92038017911c08f2772a70c2e7cfa83cec7d5e3
Simon <simonmyhre1@gmail.com>
2022-09-11 19:38:57 +0200

    docs: describe process

--- data-processing/reshape_and_augment.py
+++ data-processing/reshape_and_augment.py
@@ -1,12 +1,13 @@
 from augmenter_class import ReshaperAndAugmenter
-import itertools
 
+"""
+This file calls reshape and augmenting functions on the images in train and val folders.
+"""
 aug = ReshaperAndAugmenter()
-
 # Reshape and augment the data
 aug.reshape_and_augment("../chest-xray-balanced/train/NORMAL/", "../chest-xray-augmented/train/NORMAL/")
 aug.reshape_and_augment("../chest-xray-balanced/train/PNEUMONIA/", "../chest-xray-augmented/train/PNEUMONIA/")
 aug.reshape_and_augment("../chest-xray-balanced/val/NORMAL/", "../chest-xray-augmented/val/NORMAL/")
 aug.reshape_and_augment("../chest-xray-balanced/val/PNEUMONIA/", "../chest-xray-augmented/val/PNEUMONIA/")
 
-# The test is manually moved from the balanced test folder to the augmented test folder
+# The test images are manually moved from the balanced test folder to the augmented test folder.
