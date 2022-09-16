d32735c5754bab843d1867313028e9381d9a4d5d
Simon <simonmyhre1@gmail.com>
2022-09-16 11:26:55 +0200

    feat: change path to data
    Change the path to the new location to the data.

--- data-processing/reshape_and_augment.py
+++ data-processing/reshape_and_augment.py
@@ -5,9 +5,9 @@ This file calls reshape and augmenting functions on the images in train and val 
 """
 aug = ReshaperAndAugmenter()
 # Reshape and augment the data
-aug.reshape_and_augment("../chest-xray-balanced/train/NORMAL/", "../chest-xray-augmented/train/NORMAL/")
-aug.reshape_and_augment("../chest-xray-balanced/train/PNEUMONIA/", "../chest-xray-augmented/train/PNEUMONIA/")
-aug.reshape_and_augment("../chest-xray-balanced/val/NORMAL/", "../chest-xray-augmented/val/NORMAL/")
-aug.reshape_and_augment("../chest-xray-balanced/val/PNEUMONIA/", "../chest-xray-augmented/val/PNEUMONIA/")
+aug.reshape_and_augment("../data/chest-xray-balanced/train/NORMAL/", "../chest-xray-augmented/train/NORMAL/")
+aug.reshape_and_augment("../data/chest-xray-balanced/train/PNEUMONIA/", "../chest-xray-augmented/train/PNEUMONIA/")
+aug.reshape_and_augment("../data/chest-xray-balanced/val/NORMAL/", "../chest-xray-augmented/val/NORMAL/")
+aug.reshape_and_augment("../data/chest-xray-balanced/val/PNEUMONIA/", "../chest-xray-augmented/val/PNEUMONIA/")
 
 # The test images are manually moved from the balanced test folder to the augmented test folder.
