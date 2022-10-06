695d0596c73d6ebf1ec1697540d9125b50a1acb1
Simon <simonmyhre1@gmail.com>
2022-10-06 19:40:21 +0200

    chore: remove approach one

--- model/hyperparameter_tuning/main.py
+++ model/hyperparameter_tuning/main.py
@@ -31,86 +31,12 @@ os.mkdir(runs_dir)
 best_result_model_name = ""
 best_result = 0
 
-# Load data
-train_ds, val_ds, test_ds, class_names =  load_data(TRAIN_PATH, VAL_PATH, TEST_PATH, IMG_SHAPE, 32)
-
-"""
-Hyperparameter tuning the model from approach one
-"""
-
-i = 0
-# Four layers of dense
-for first_conv_layer_filter in CONV_FILTERs:
-    for second_conv_layer_filter in CONV_FILTERs:
-        for third_conv_layer_filter in CONV_FILTERs:
-            for fourth_conv_layer_filter in CONV_FILTERs:
-                # Two layers of dense
-                for first_dense_layer_filter in DENSE_FILTERs:
-                    for second_dense_layer_filter in DENSE_FILTERs:
-                        print("\n\nIteration: {}\n".format(i))
-                        i += 1
-                        name = "conv-{}-{}-{}-{}-dense-{}-{}".format(first_conv_layer_filter, second_conv_layer_filter, third_conv_layer_filter, fourth_conv_layer_filter, first_dense_layer_filter, second_dense_layer_filter)
-
-                        model = Sequential([
-                            Input(shape=IMG_SHAPE),
-                            Rescaling(1. / 255),
-                            Conv2D(first_conv_layer_filter, 3, activation='relu'),
-                            MaxPooling2D(),
-                            Conv2D(second_conv_layer_filter, 3, activation='relu'),
-                            MaxPooling2D(),
-                            Conv2D(third_conv_layer_filter, 3, activation='relu'),
-                            Dropout(0.2),
-                            MaxPooling2D(),
-                            Conv2D(fourth_conv_layer_filter, 3, activation='relu'),
-                            Flatten(),
-                            Dense(first_dense_layer_filter, activation='relu'),
-                            Dropout(0.2),
-                            Dense(second_dense_layer_filter, activation='relu'),
-                            Dense(1, activation='sigmoid')
-                        ])
-
-                        model.compile(
-                            optimizer=Adam(learning_rate=LEARNING_RATE),
-                            loss='binary_crossentropy',
-                            metrics=['accuracy']
-                        )
-                        model._name = name
-
-                        # Set callbacks
-                        early_stopping = EarlyStopping(
-                            monitor='val_loss',
-                            patience=3
-                        )
-                        logs_dir = "{}/tensorboard/{}".format('model_runs', model.name)
-                        tensorboard = TensorBoard(
-                            log_dir=logs_dir
-                        )
-
-                        # Train model
-                        history = model.fit(
-                            train_ds,
-                            validation_data=val_ds,
-                            epochs=EPOCHS,
-                            callbacks=[early_stopping, tensorboard]
-                        )
-
-                        # Evaluate model
-                        test_results = model.evaluate(test_ds)
-                        print("Test results: Loss: {}, Accuracy: {}".format(test_results[0], test_results[1]))
-
-                        # Save results
-                        save_results(test_results, model.name, runs_dir)
-
-                        if test_results[1] > best_result:
-                            best_result_model_name = model.name
-                            best_result = test_results[1]
-
 
 """
 Hyperparameter tuning the model from approach two
 """
 
-
+train_ds, val_ds, test_ds, class_names =  load_data(TRAIN_PATH, VAL_PATH, TEST_PATH, IMG_SHAPE_FT, 32)
 
 extractor = InceptionV3(input_shape=IMG_SHAPE_FT,
                                 include_top=False,
@@ -120,7 +46,7 @@ layer_name = 'mixed2'
 
 extractor_cut = Model(inputs=extractor.input, outputs=extractor.get_layer(layer_name).output)
 
-
+i = 0
 # Two layers of conv
 for first_conv_layer_filter in CONV_FILTERs:
     for second_conv_layer_filter in CONV_FILTERs:
@@ -149,6 +75,8 @@ for first_conv_layer_filter in CONV_FILTERs:
                     metrics=['accuracy']
                 )
                 model._name = name
+
+                model.summary()
 
                 # Set callbacks
                 early_stopping = EarlyStopping(
