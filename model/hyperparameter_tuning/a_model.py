7eb74f12f3a7d49ac10ecfac06b5e1a35ae700c6
Simon <simonmyhre1@gmail.com>
2022-10-08 20:37:02 +0200

    feat: predict some selected inputs

--- model/hyperparameter_tuning/a_model.py
+++ model/hyperparameter_tuning/a_model.py
@@ -1,5 +1,6 @@
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import image_dataset_from_directory
+import numpy as np
 
 test_ds = image_dataset_from_directory(
         "../../data/chest-xray-augmented/test",
@@ -20,6 +21,26 @@ predicitons = model.predict(test_ds)
 # Save predictions to a file
 import numpy as np
 np.savetxt("predictions.csv", predicitons, delimiter=",")
+
+
+"""
+Some selected inputs task
+"""
+some_selected_inputs_ds = image_dataset_from_directory(
+        "../../data/some-selected-inputs",
+        image_size=(224, 224),
+        batch_size=1,
+        color_mode='rgb',
+        shuffle=False
+    )
+
+print("\nSome selected inputs predictions:")
+
+# Take one image at the time from dataset and print a prediction and label
+for image, label in some_selected_inputs_ds:
+    print("Prediction: ", model.predict(image))
+    print("Actual: ", label.numpy())
+    print("")
 
 
 
